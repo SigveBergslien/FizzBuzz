951ad0d2ed57b12369c5cec859d1dabb32af5dad
SigveBergslien <sigvebergslien@hotmail.com>
2012-06-18 18:33:30 +0200

    First commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            utility.fizzBuzz1_100();
+            String exit = Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    static class utility
+    {
+        public static void fizzBuzz(int i)
+        {
+            Console.Write(i + " ");
+            if (i % 3 == 0)
+                Console.Write("Fizz");
+            if (i % 5 == 0)
+               Console.Write("Buzz");
+            Console.WriteLine();
+        }
+        public static void fizzBuzz1_100()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                fizzBuzz(i);
+            }
+        }
+    }
+}
