3301d5473c59951e4a957147ff59f7ad1f930469
SigveBergslien <sigvebergslien@hotmail.com>
2012-06-19 10:25:38 +0200

    Final

--- ConsoleApplication1/utility.cs
+++ ConsoleApplication1/utility.cs
@@ -11,19 +11,10 @@ namespace ConsoleApplication1
         {
             Console.Write(i + " ");
             if (i % 3 == 0)
-            {
                 Console.Write("Fizz");
-                if (((i / 3)%1) != 0)
-                    Console.Write("ERROR");
-            }
             if (i % 5 == 0)
-            {
                 Console.Write("Buzz");
-                if (((i / 5)%1)!= 0)
-                    Console.Write("ERROR");
-            }
             Console.WriteLine();
-        }
         public static void fizzBuzz1_100()
         {
             for (int i = 1; i < 101; i++)
